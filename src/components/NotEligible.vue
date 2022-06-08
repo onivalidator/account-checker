f65e8cf08b0db4a7dc4a7c9f449202e0d6669ee7
Jean-Emrick Rioux <gestionjerioux@gmail.com>
2022-06-08 19:38:00 -0400

    refactor for evmos

---
+++ src/components/NotEligible.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div class="text-body-1">Sorry fren, keep cooking to be eligible for the 50 or even the 100 EVMOS giveaway. <br> You need to delegate at least
+            25 EVMOS to Stake Frites 🥩 🍟 </div>
+        <div class="mt-5">
+            <v-btn
+            target="__blank__"
+            href="https://wallet.keplr.app/#/evmos/stake?modal=stake&validator=evmosvaloper1pz3mcahcrglf3md4lggax5r95gvmppc6x5w7hw">
+            Delegate on KEPLR</v-btn>
+            <v-btn flat color="primary" class="ml-3"
+            target="__blank__"
+            href="https://restake.app/evmos/evmosvaloper1pz3mcahcrglf3md4lggax5r95gvmppc6x5w7hw">
+            Delegate on Restake</v-btn>
+        </div>
+        <div class="text-body-1 my-5">Would you love to get <strong>100 JUNO</strong> instead of 50?</div>
+        <div class="text-body-2">Delegate 5 ATOM to Stake Frites </div>
+        <div class="mt-5">
+            <v-btn
+            target="__blank__"
+            href="https://wallet.keplr.app/#/evmos/stake?modal=stake&validator=evmosvaloper1pz3mcahcrglf3md4lggax5r95gvmppc6x5w7hw">
+            Delegate on KEPLR</v-btn>
+            <v-btn flat color="primary" class="ml-3"
+            target="__blank__"
+            href="https://restake.app/evmos/evmosvaloper1pz3mcahcrglf3md4lggax5r95gvmppc6x5w7hw">
+            Delegate on Restake</v-btn>
+        </div>
+    </div>
+</template>
